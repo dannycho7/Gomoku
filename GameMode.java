4fafc4562a3c963b6cf4dc53328b8c29225b1676
dannycho7 <dannycho7@gmail.com>
2016-10-01 17:05:09 -0700

    Add files via upload

---
+++ GameMode.java
@@ -0,0 +1,22 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class GameMode here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class GameMode extends World
+{
+    public static int count = 0;
+    /**
+     * Constructor for objects of class GameMode.
+     * 
+     */
+    public GameMode()
+    {    
+        // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
+        super(600, 400, 1); 
+        
+    }
+}
